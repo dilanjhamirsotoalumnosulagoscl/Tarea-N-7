25bae29121bcf5e35379f11f3bb64aa34bcc5260
dilanjhamirsotoalumnosulagoscl <dilanjhamir.soto@alumnos.ulagos.cl>
2024-06-25 11:49:10 -0400

    Update Deudores.py

--- Deudores.py
+++ Deudores.py
@@ -1,13 +1,10 @@
-# Creamos un diccionario vacío para almacenar los deudores
 deudores = {}
 
-# Creamos 5 deudores con sus respectivas deudas
 for i in range(5):
     rut = input("Ingrese el RUT de la persona ")
     deuda = int(input("Ingrese la deuda de la persona "))
     deudores[rut] = deuda
 
-# Bucle para ingresar abonos a las deudas
 while True:
     rut = input("Ingrese el RUT de la persona a abonar (o presione Enter para finalizar) ")
     if rut == "":
@@ -20,7 +17,6 @@ while True:
         if deudores[rut] <= 0:
             del deudores[rut]
 
-# Mostramos las personas que quedaron deudoras y sus respectivos saldos de deuda
 print("Personas que quedaron deudoras y sus respectivos saldos de deuda:")
 for rut in deudores:
     print("RUT: " + rut + ", Deuda: " + str(deudores[rut]))
